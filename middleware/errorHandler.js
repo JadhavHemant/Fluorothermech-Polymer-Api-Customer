06be587b4a43c06c456fe44d608fc49e283911af
hemant <hemantjadhav7477@gmail.com>
2025-03-27 23:05:40 +0530

    done apis

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,12 @@
-const errorHandler = (err, req, res, next) => {
-    res.status(500).json({ message: err.message });
-  };
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   
-  module.exports = errorHandler;
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+  }
   
+  next();
+});
